be85d82a41e1fc6405f845a987d4fcd97ad173e5
lyq525 <1985731645@qq.com>
2025-07-15 10:32:57 +0800

    Add files via upload

---
+++ homework01/shellscripts.sh
@@ -0,0 +1,14 @@
+mkdir -p linux_practice/name
+mkdir -p linux_practice/permission
+touch linux_practice/name/file1.txt
+touch linux_practice/name/file2.txt
+touch linux_practice/permission/file3.txt
+touch linux_practice/permission/file4.txt
+rm linux_practice/name/file1.txt
+mv linux_practice/name/file2.txt linux_practice/name/show.txt
+echo "Hello linux" > linux_practice/name/show.txt
+cat linux_practice/name/show.txt
+chmod 644 linux_practice/permission/file*.txt
+chmod 644 linux_practice/permission/file*.txt
+echo "Changed permissions for file3.txt to -rw-r--r--"
+echo "Changed permissions for file4.txt to -rw-r--r--"
