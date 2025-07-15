51073521d352d2d42ad4a063bebe55cbb8235b5c
lyq525 <1985731645@qq.com>
2025-07-15 10:39:03 +0800

    Update shellscripts.sh

--- homework01/shellscripts.sh
+++ homework01/shellscripts.sh
@@ -9,6 +9,5 @@ mv linux_practice/name/file2.txt linux_practice/name/show.txt
 echo "Hello linux" > linux_practice/name/show.txt
 cat linux_practice/name/show.txt
 chmod 644 linux_practice/permission/file*.txt
-chmod 644 linux_practice/permission/file*.txt
 echo "Changed permissions for file3.txt to -rw-r--r--"
 echo "Changed permissions for file4.txt to -rw-r--r--"
