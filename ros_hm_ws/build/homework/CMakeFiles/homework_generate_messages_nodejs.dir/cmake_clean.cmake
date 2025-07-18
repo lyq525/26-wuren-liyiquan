7f7888238c3523ebb3ebb5279133cefec49dbb69
lyq525 <1985731645@qq.com>
2025-07-18 22:46:08 +0800

    Add files via upload

---
+++ ros_hm_ws/build/homework/CMakeFiles/homework_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/lyq/ros_hm_ws/devel/share/gennodejs/ros/homework/msg/linearVelocity.js"
+  "CMakeFiles/homework_generate_messages_nodejs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/homework_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
