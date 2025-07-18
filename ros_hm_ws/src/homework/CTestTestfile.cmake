ca2c28647183d4cea6047282b2e9475a3dec1522
lyq525 <1985731645@qq.com>
2025-07-18 22:57:12 +0800

    Add files via upload

---
+++ ros_hm_ws/src/homework/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lyq/ros_hm_ws/src
+# Build directory: /home/lyq/ros_hm_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("homework")
