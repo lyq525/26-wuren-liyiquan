9b16472a3e6acd6889d15ade54c21b20d2536999
lyq525 <1985731645@qq.com>
2025-07-18 22:38:01 +0800

    Add files via upload

---
+++ ros_hm_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/lyq/ros_hm_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/lyq/ros_hm_ws/devel;/opt/ros/noetic'.split(';') if '/home/lyq/ros_hm_ws/devel;/opt/ros/noetic' != '' else []
