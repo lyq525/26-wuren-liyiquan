0ff2d2fcc63883c5c0b771a6859b084e4b309cd0
lyq525 <1985731645@qq.com>
2025-07-20 01:09:46 +0800

    Add files via upload

---
+++ ros_advanced_homework/cone_visualizer/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/lyq/cone_visualizer/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
---
+++ ros_advanced_homework/cone_visualizer/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lyq/cone_visualizer/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/lyq/cone_visualizer/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/lyq/cone_visualizer/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/lyq/cone_visualizer/build'
+export PYTHONPATH="/home/lyq/cone_visualizer/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lyq/cone_visualizer/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/lyq/cone_visualizer/src:$ROS_PACKAGE_PATH"
