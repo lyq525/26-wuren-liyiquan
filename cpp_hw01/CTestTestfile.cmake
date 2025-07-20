06ac5ed51b1e71b2e0a1eb3913fe8e7259a9ccbe
lyq525 <1985731645@qq.com>
2025-07-21 01:17:14 +0800

    Add files via upload

---
+++ cpp_hw01/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lyq/cpp_homework_01/src/cpp_hw01
+# Build directory: /home/lyq/cpp_homework_01/build/cpp_hw01
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
