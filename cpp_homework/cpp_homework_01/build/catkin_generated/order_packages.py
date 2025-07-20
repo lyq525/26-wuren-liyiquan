85b9c3d67f25d11215f1dfa6c62b1901b720f1e2
lyq525 <1985731645@qq.com>
2025-07-21 01:05:21 +0800

    Add files via upload

---
+++ cpp_homework/cpp_homework_01/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/lyq/cpp_homework_01/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/lyq/cpp_homework_01/devel;/opt/ros/noetic'.split(';') if '/home/lyq/cpp_homework_01/devel;/opt/ros/noetic' != '' else []
