8bbde9f17176e165fe4f7f9992867d4c001695da
lyq525 <1985731645@qq.com>
2025-07-21 02:14:15 +0800

    Delete cpp_hw01 directory

--- cpp_hw01/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;sensor_msgs;cv_bridge;std_msgs;fsd_common_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "cpp_hw01"
-PROJECT_SPACE_DIR = "/home/lyq/cpp_homework_01/install"
-PROJECT_VERSION = "0.0.0"
