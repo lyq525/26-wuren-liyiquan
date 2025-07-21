3c28f15a14f4483912120653e5861f0e98797fc9
Li Yiquan <1985731645@qq.com>
2025-07-21 15:43:46 +0800

    Add files via upload

---
+++ cpp_homework/cpp_homework_02/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/lyq/cpp_homework_02/src
+# Build directory: /home/lyq/cpp_homework_02/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("fsd_common_msgs")
+subdirs("pointcloud_merging")
