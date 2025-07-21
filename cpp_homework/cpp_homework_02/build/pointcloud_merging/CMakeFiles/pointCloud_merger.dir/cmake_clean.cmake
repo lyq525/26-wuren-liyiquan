9ea83ad13015e84a751782a74590db8521d75513
Li Yiquan <1985731645@qq.com>
2025-07-21 16:30:03 +0800

    Add files via upload

---
+++ cpp_homework/cpp_homework_02/build/pointcloud_merging/CMakeFiles/pointCloud_merger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lyq/cpp_homework_02/devel/lib/pointcloud_merging/pointCloud_merger"
+  "/home/lyq/cpp_homework_02/devel/lib/pointcloud_merging/pointCloud_merger.pdb"
+  "CMakeFiles/pointCloud_merger.dir/src/pointCloud_merger.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointCloud_merger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
