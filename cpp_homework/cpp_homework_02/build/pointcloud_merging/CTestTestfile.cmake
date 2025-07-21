c37243c26255914df4cb0bd37a2cd23c299bdee7
Li Yiquan <1985731645@qq.com>
2025-07-21 16:11:08 +0800

    Add files via upload

---
+++ cpp_homework/cpp_homework_02/build/pointcloud_merging/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lyq/cpp_homework_02/src/pointcloud_merging
+# Build directory: /home/lyq/cpp_homework_02/build/pointcloud_merging
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ cpp_homework/cpp_homework_02/build/pointcloud_merging/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "fsd_common_msgs;roscpp;std_msgs;pcl_ros;pcl_conversions".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "pointcloud_merging"
+PROJECT_SPACE_DIR = "/home/lyq/cpp_homework_02/devel"
+PROJECT_VERSION = "0.0.0"
